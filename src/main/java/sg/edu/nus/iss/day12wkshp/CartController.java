d35c49cc175a3f7e2ca9ef094d9ebe3a986a7d55
kayu <your_email>
2022-07-19 14:33:20 +0800

    pathvariable test in CartController

--- src/main/java/sg/edu/nus/iss/day12wkshp/CartController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/CartController.java
@@ -1,10 +1,12 @@
 package sg.edu.nus.iss.day12wkshp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //import sg.edu.nus.iss.day12wkshp.CartService;
@@ -21,5 +23,24 @@ public class CartController {
         model.addAttribute("cart", cartItems);
 
         return "cart"; // sensitive, referring to cart.html //inject into view
+    }
+
+    @GetMapping("{itemname}")
+    public String filteredCart(
+            @PathVariable(name = "itemname", required = true) String itemname, //pathvariable = weather/Singapore
+            Model model) {
+        CartService cs = new CartService();
+        List<Item> cartItems = cs.getShoppingItems();
+
+        List<Item> filtereItems = new ArrayList<>();
+        for (Item s : cartItems) {
+            if (s.getItemName().contains(itemname)) {
+                filtereItems.add(s);
+            }
+        }
+
+        model.addAttribute("cart", cartItems);
+
+        return "cart";
     }
 }
