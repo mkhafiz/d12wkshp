157059aaa6704341b7b7f3de16caf4459c6108a0
kayu <your_email>
2022-07-19 14:35:57 +0800

    Include filtered items (LN42) for CartController

--- src/main/java/sg/edu/nus/iss/day12wkshp/CartController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/CartController.java
@@ -39,7 +39,7 @@ public class CartController {
             }
         }
 
-        model.addAttribute("cart", cartItems);
+        model.addAttribute("cart", filtereItems);
 
         return "cart";
     }
