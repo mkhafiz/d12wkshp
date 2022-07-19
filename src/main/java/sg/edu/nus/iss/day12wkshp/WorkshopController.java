56977079e098fa45b7118f1a91b5b31837debd4d
kayu <your_email>
2022-07-19 16:25:55 +0800

    Added workshopresult.html

--- src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
@@ -1,14 +1,49 @@
 package sg.edu.nus.iss.day12wkshp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WorkshopController {
 
-    @RequestMapping(path={"/workshop"})
+    @RequestMapping(path = { "/workshop" })
     public String test() {
         return "workshop";
     }
-    
+
+    @RequestMapping(path = { "/processNumber" })
+    public String testResults(
+            @RequestParam(name = "inputNumber", defaultValue = "1") Integer unit,
+            Model model) {
+
+        List<Integer> lstInt = new ArrayList<>();
+        int loopValue = Integer.parseInt(unit.toString());
+
+        int loop = 1;
+        while (loop <= loopValue) {
+
+            int result = (int) (Math.random() * loopValue + 1);
+
+            if (!lstInt.contains(Integer.valueOf(result))) {
+                lstInt.add(Integer.valueOf(result));
+                loop++;
+            }
+        }
+
+        for (int i = 0; i < lstInt.size(); i++) {
+            System.out.println("--> " + lstInt.get(i));
+            System.out.println();
+        }
+
+        model.addAttribute("numbers", lstInt);
+
+        return "workshopresult";
+
+    }
+
 }
