6b93905ad0bce1935a0489254176b7b2d3ce24a3
kayu <your_email>
2022-07-19 12:05:30 +0800

    Added CartController

---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/CartController.java
@@ -0,0 +1,24 @@
+package sg.edu.nus.iss.day12wkshp;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sg.edu.nus.iss.day12wkshp.CartService;
+
+@Controller
+@RequestMapping(path = { "/cart" })
+public class CartController {
+
+    @GetMapping(produces = { "text/html" })
+    public String displayCart(Model model) {
+        CartService cs = new CartService();
+        List<item> cartItems = cs.getShoppingItems();
+        model.addAttribute("cart", cartItems);
+
+        return "cart";
+    }
+}
